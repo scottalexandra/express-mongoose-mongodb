d38a7764d6b333af7c4fd5efab8618f9abfa7506
Alex Robinson <alexandra.scott335@gmail.com>
2017-11-28 15:50:30 -0700

    setup port

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger('dev'));
@@ -14,10 +12,6 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Welcome to my api');
-});
-
-app.listen(port, () => {
-  console.log('Running on PORT: ' + port);
 });
 
 module.exports = app;
