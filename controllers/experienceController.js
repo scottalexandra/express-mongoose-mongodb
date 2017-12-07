d67c421bbb23c1b728c754330f00878c1697c4a3
Alex Robinson <alexandra.scott335@gmail.com>
2017-12-06 22:25:47 -0700

    WIP experience routes and controller

---
+++ controllers/experienceController.js
@@ -0,0 +1,5 @@
+const experienceModel = require('../models/experienceModel');
+
+exports.create = (req, res) => {
+  res.json('TODO create an experience by POSTing');
+};
---
+++ routes/experience.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const experienceController = require('../controllers/experienceController');
+
+router.post('/experiences');
